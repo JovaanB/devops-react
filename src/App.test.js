77198ea6ec6744dee48cc8d27d9af080700b64fe
Jovan <bienvenujovan@gmail.com>
2023-01-05 15:08:28 +0100

    Update test for app.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders With Github Actions paragraph", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const pElement = screen.getByText(/With Github Actions/i);
+  expect(pElement).toBeInTheDocument();
 });
