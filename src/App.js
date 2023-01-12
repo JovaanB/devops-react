a57dc190fdc810b77c976f3243eb1cca986e6ffb
Jovan <bienvenujovan@gmail.com>
2023-01-12 15:31:03 +0100

    Failed tests

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Devops React</h1>
-        <p>With Github Actions</p>
+        <p>With Github Actions!</p>
         <ul>
           <li>Jovan BIENVENU</li>
           <li>ENIGMA 5ème année</li>
